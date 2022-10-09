20c600f1453716afdb5e5a7182301477ffc1d839
rashhhhh <93440201+rashhhhh@users.noreply.github.com>
2022-10-10 01:03:45 +0530

    Create Calculator.c

---
+++ Programs/C/Calculator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+  int num1, num2;
+  char act;
+  printf("Enter First Number : ");
+  scanf("%d", &num1);
+  printf("Enter Second Number : ");
+  scanf("%d", &num2);
+  printf("Enter Desired Action : ");
+  scanf("%d", &act);
+  switch (act)
+  {
+  case '+':
+    printf("SUM of the two numbers %d and %d : %d\n", num1, num2, num1 + num2);
+    break;
+  case '-':
+    printf("SUB of the two numbers %d and %d : %d\n", num1, num2, num1 - num2);
+    break;
+  case '*':
+    printf("MUL of the two numbers %d and %d : %d\n", num1, num2, num1 * num2);
+    break;
+  case '/':
+    printf("DIV of the two numbers %d and %d : %d\n", num1, num2, num1 / num2);
+    break;
+  default:
+    printf("Invalid Action!\n");
+    break;
+  }
+  return 0;
+}
