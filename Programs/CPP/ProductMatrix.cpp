3fb7f2e9ac79c0c0e045ff5d1bf5be12abc5f434
Saumya-Shrivastav <69923632+Saumya-Shrivastav@users.noreply.github.com>
2022-10-09 16:39:17 +0530

    Create ProductMatrix.cpp

---
+++ Programs/CPP/ProductMatrix.cpp
@@ -0,0 +1,60 @@
+// Product of 2 Matrix
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    float a[10][10], b[10][10], c[10][10];
+    int m,n,p,q;
+    cout<<"Order of the First Matrix";
+    cout<<"\nRows= "; cin>>m;
+    cout<<"\nColumns= "; cin>>n;
+    cout<<"\nOrder of the Second Matrix";
+    cout<<"\nRows= "; cin>>p;
+    cout<<"\nColumns= "; cin>>q;
+    if(n!=p)
+    {
+        cout<<"\nProduct not possible";
+        cout<<"\nTry Again...!";
+        exit(0);
+    }
+    cout<<"Read the First Matrix ";
+    int i,j,k;
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    cout<<"Read the Second Matrix ";
+    for(i=0; i<p; i++)
+    {
+        for(j=0; j<q; j++)
+        {
+            cin>>b[i][j];
+        }
+    }
+    for(i=0;i<m;i++)
+    {
+        for(k=0;k<q;k++)
+        {
+            c[i][k]=0;
+            for(j=0;j<n;j++)
+            {
+                c[i][k]+= a[i][j]*b[j][k];
+            }
+        }
+    }
+    cout<<"Product is ";
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<q; j++)
+        {
+            cout<<c[i][j]<<"\n"<<endl;
+        }
+    }
+    return 0;
+}
